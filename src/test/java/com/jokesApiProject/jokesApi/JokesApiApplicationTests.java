f751b02fb9f4294eb2ed335c60c7f67a61ca5e7d
saisumanth <saisumanth0531@gmail.com>
2023-04-11 17:33:40 +0530

    fetch data using java-1

---
+++ src/test/java/com/jokesApiProject/jokesApi/JokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokesApiProject.jokesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
