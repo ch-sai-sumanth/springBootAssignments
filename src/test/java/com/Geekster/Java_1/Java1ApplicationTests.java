44bfa18a631aa9947d8e5c2a00b101236996b76c
saisumanth <saisumanth0531@gmail.com>
2023-04-11 17:54:40 +0530

    fetch data using java

---
+++ src/test/java/com/Geekster/Java_1/Java1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Geekster.Java_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
